aa9353be8b0f86e28e5addb1510e0cf1c35135e8
giorgi chilly <gchilingarasvili@gmail.com>
2019-02-16 20:10:22 +0400

    views add

--- suar_travel/suar_travel/tours/views.py
+++ suar_travel/suar_travel/tours/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views import generic
+
 
 # Create your views here.
+from .models import Tour
+
+
+class Home(generic.ListView):
+	model = Tour
+
+	template_name = 'index.html'
