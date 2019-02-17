65aa74f4a02957f5ee827b40ab306f9a3b6d0d2f
tengiz <chtengo@gmail.com>
2019-02-17 16:53:53 +0200

    fixed

--- suar_travel/suar_travel/tours/views.py
+++ suar_travel/suar_travel/tours/views.py
@@ -6,28 +6,26 @@ from .models import Tour, Images
 
 
 class Home(generic.ListView):
-	model = Tour
+    model = Tour
 
-	template_name = 'index.html'
+    template_name = 'index.html'
 
 
-class TourForms(generic.DetailView):
-	model = Tour
-	template_name = 'create_tour.html'
+class TourForms(generic.ListView):
+    model = Tour
+    template_name = 'create_tour.html'
 
-	def get(self, request, *args, **kwargs):
-		ImageFormSet = modelformset_factory(Images, form=form.ImageForm, extra=3)
-		self.tourform = form.ToursForm()
-		self.formset = ImageFormSet(queryset=Images.objects.none())
-		context = self.get_context_data(object=self.object)
-		return self.render_to_response(context)
+    def get_queryset(self):
+        ImageFormSet = modelformset_factory(Images, form=form.ImageForm, extra=3)
+        self.tourform = form.ToursForm()
+        self.formset = ImageFormSet(queryset=Images.objects.none())
 
-	def get_context_data(self, **kwargs):
-		context = super().get_context_data(**kwargs)
-		context['tour_form'] = self.tourform
-		context['image_form'] = self.formset
-		return context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tour_form'] = self.tourform
+        context['image_form'] = self.formset
+        return context
 
 
 class TourCreate(generic.CreateView):
-	pass
+    pass
