b72ed7e7f4a0b2223ff975f4b3b199f9e3ddc143
tengiz <chtengo@gmail.com>
2019-03-02 14:29:44 +0200

    fixed image size

--- suar_travel/suar_travel/tours/views.py
+++ suar_travel/suar_travel/tours/views.py
@@ -7,65 +7,66 @@ from .models import Tour, Images
 
 
 class Home(generic.ListView):
-	model = Tour
-	template_name = 'index.html'
-	context_object_name = "tours"
+    model = Tour
+    template_name = 'index.html'
+    context_object_name = "tours"
 
 
 class TourCreate(generic.FormView, LoginRequiredMixin, PermissionRequiredMixin):
-	permission_required = 'add_tour'
-	template_name = 'create_tour.html'
+    permission_required = 'add_tour'
+    template_name = 'create_tour.html'
 
-	@transaction.atomic
-	def post(self, request, *args, **kwargs):
-		context = {}
-		tourform = ToursForm(request.POST, request.FILES)
-		if tourform.is_valid():
-			try:
-				tour_form = tourform.save(commit=True)
-			except:
-				print("error")
+    @transaction.atomic
+    def post(self, request, *args, **kwargs):
+        context = {}
+        tourform = ToursForm(request.POST, request.FILES)
+        if tourform.is_valid():
+            try:
+                tour_form = tourform.save(commit=True)
+            except:
+                print("error")
 
-			for file in request.FILES.getlist('image'):
-				image = Images()
-				image.tour = tour_form
-				image.image.save(name=file.name, content=file)
-				image.save()
+            for file in request.FILES.getlist('image'):
+                image = Images()
+                image.tour = tour_form
+                image.image.save(name=file.name, content=file)
+                image.save()
 
-			for file in request.FILES.getlist('main_image'):
-				image = Images()
-				image.is_main = True
-				image.tour = tour_form
-				image.image.save(name=file.name, content=file)
-				image.save()
+            for file in request.FILES.getlist('main_image'):
+                image = Images()
+                image.is_main = True
+                image.tour = tour_form
+                image.image.save(name=file.name, content=file)
+                image.save()
 
-			context['success'] = True
-		else:
-			print(tourform.errors)
-			for err in tourform.errors:
-				context['error'] = tourform[err].errors[0]
-		return self.render_to_response(context)
+            context['success'] = True
+        else:
+            print(tourform.errors)
+            for err in tourform.errors:
+                context['error'] = tourform[err].errors[0]
+        return self.render_to_response(context)
 
-	def get(self, request, *args, **kwargs):
-		context = {
-			'tour_form': ToursForm(),
-			'image_form': ImageForm(),
-			'MainImageForm': MainImageForm()
-		}
-		return self.render_to_response(context)
+    def get(self, request, *args, **kwargs):
+        context = {
+            'tour_form': ToursForm(),
+            'image_form': ImageForm(),
+            'MainImageForm': MainImageForm()
+        }
+        return self.render_to_response(context)
 
 
-class Destination(generic.ListView,SelectRelatedMixin):
-	template_name = 'destination.html'
-	model = Tour
-	# select_related = ['images']
-	context_object_name = "tours"
+class Destination(generic.ListView, SelectRelatedMixin):
+    template_name = 'destination.html'
+    model = Tour
+    # select_related = ['images']
+    context_object_name = "tours"
 
-	# def get(self, request, *args, **kwargs):
-	# 	k=request.objects.all()
-	# 	print(k)
+# def get(self, request, *args, **kwargs):
+# 	k=request.objects.all()
+# 	print(k)
+
 
 class TourDetail(generic.DetailView):
-	model = Tour
-	template_name = 'tour_detail.html'
-	context_object_name = 'tour'
+    model = Tour
+    template_name = 'tour_detail.html'
+    context_object_name = 'tour'
